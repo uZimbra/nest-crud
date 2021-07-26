3dc5d8920facb427fcb3dcba2591757cdfd03c7b
Rodrigo A. Silva <rgbrasil2@gmail.com>
2021-07-26 20:16:24 -0300

    chore: enviroment file configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { config } from 'dotenv';
+config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.API_PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(port, () => {
+    console.log(`\x1b[32mready\x1b[0m - started server on port ${port}`);
+  });
 }
+
 bootstrap();
